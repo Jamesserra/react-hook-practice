fa3c971a83a687d1741c1cc67de3117acf2e1410
jamesserra <jamesserra4@gmail.com>
2022-05-10 08:02:03 -0400

    display robots

--- src/CardList.js
+++ src/CardList.js
@@ -1,23 +1,24 @@
 import React from "react";
 import Card from "./Card";
-import {robots} from './Robots'
 
-let y = 0;
-const random = () => {
-    let x = Math.floor(Math.random() * robots.length)
-    y = x
-    return y
-}
-
-
-const CardList = () => {
-    return (
-        <div>
-            <Card id={robots[random(y)].id} name={robots[y.name]} email={y.email}/>
-            <Card id={robots[random(y)].id} name={robots[y.name]} email={y.email}/>
-            <Card id={robots[random(y)].id} name={robots[y.name]} email={y.email}/>
-        </div>
-    );
+const CardList = ({robots}) => {
+  return (            
+    <div>               
+      {
+        robots.map((user, i) => {
+          return (
+            <Card 
+              key={robots[i].id} 
+              id={robots[i].id} 
+              name={robots[i].name} 
+              email={robots[i].email}
+              />
+            );
+          })
+        }       
+    </div>
+  );
 }
 
 export default CardList
+             
